3b9c875b206257bbfae61171b6202b2b609b53c9
Nikolay1411 <Astben2020@mail.ru>
2023-07-07 20:56:44 +0400

    car

---
+++ app/core/Router.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\core;
+class Router
+{
+    protected $params = [];
+    public function match()
+    {
+        $url = trim($)
+    }
+
+    public function run()
+    {
+        $this->match();
+    }
+
+
+}
